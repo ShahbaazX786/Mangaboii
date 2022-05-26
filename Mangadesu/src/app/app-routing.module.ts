b8993220dc0a9afe6cdc5cd3434e8e6c5c95153a
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-05-26 09:53:15 +0530

    CL-1.0.4 : Added Basic header and routing paths.

--- Mangadesu/src/app/app-routing.module.ts
+++ Mangadesu/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
+import { ListComponent } from './components/list/list.component';
+import { SearchComponent } from './components/search/search.component';
+import { RulesComponent } from './components/rules/rules.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { AboutComponent } from './components/about/about.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { PnfComponent } from './components/pnf/pnf.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'',redirectTo:'home', pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactUsComponent},
+  {path:'rules',component:RulesComponent},
+  {path:'search',component:SearchComponent},
+  {path:'list',component:ListComponent},//I dont think this component is of any use.
+  {path:'**',component:PnfComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
