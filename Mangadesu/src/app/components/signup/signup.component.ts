c133d818bbd8cf3d8e8d891e00ce3d4b53674156
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-06-13 15:32:06 +0530

    CL-1.1.11: Added Signup-page UI

--- Mangadesu/src/app/components/signup/signup.component.ts
+++ Mangadesu/src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  signupForm!: FormGroup;
+  hide:boolean = true;
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      username:new FormControl('',[Validators.required, Validators.minLength(8), Validators.maxLength(20)]),
+      email:new FormControl('',[Validators.required, Validators.email]),
+      password:new FormControl('',[Validators.required, Validators.minLength(8), Validators.maxLength(20)]),
+    })
+  }
+
+  onSignup(){
+    console.log('uWu');
   }
 
 }
